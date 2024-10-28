03092fed4f199543dfa03f818e1eb14806c76948
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-28 14:47:23 +0100

    add: bonus

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ console.log("Hello node js");
 const pin = process.env.PIN;
 console.log(pin);
 
-const argument = process.argv.slice(2);
+const argument = process.argv.slice(2).toString();
 console.log(argument);
 
+if (argument === "admin" && pin === "666"){
+    console.log("Welcome Admin");
+} else {
+    console.log("Access restricted");
+}
